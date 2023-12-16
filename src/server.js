59c7bab26b1a9b5253f29435cb61efeff2f6886b
Fabricio Veronez <fabricioveronez@gmail.com>
2023-12-16 09:58:17 -0300

    Adicionando chave pra dar erro

--- src/server.js
+++ src/server.js
@@ -11,4 +11,7 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+const chave = "AKIA5L3DP4PBTAABXFNO"
+const chave_secreta = "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"
+
 app.listen(PORT, () => console.log(`Aplicação em execução na porta ${PORT}`));
